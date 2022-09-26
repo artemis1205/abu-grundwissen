c816ccc2699d2716766c1ca3f67ddea1299b5b1b
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-09-26 14:46:10 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css' 
 import One from '../public/1.png';
 import Three from '../public/9.png';
 import Four from '../public/4.png';
@@ -14,9 +14,7 @@ import Eleven from '../public/11.png';
 import Twelve from '../public/12.png';
 import Thirteen from '../public/13.png';
 
-
 export default function Home() {
-
 
   const [image, setImage] = useState(Three);
   const [text, setText] = useState();
@@ -24,40 +22,38 @@ export default function Home() {
   const [start, setStart] = useState(false);
 
   const images_arr = [
-  { image: One, text: "Sie kennen Grundbedürfnis, Wahlbedürfnis, Individualbedürfnis und Kollektivbedürfnis voneinander unterscheiden." },
-  { image: Three, text: "Sie können die Entstehung eines Vertrages erklären." },
-  { image: Four, text: "Sie kennen vier Vertragsformen nennen."},
-  { image: Five, text: "Sie können drei Vertragsarten mit Rücktrittsrecht nennen." },
-  { image: Six, text: "Sie können das Vorgehen bei Lieferverzug und die rechtlichen Möglichkeiten bei Zahlungsverzug aufzählen."},
-  { image: Seven, text: "Sie kennen drei Forderungsmöglichkeiten, die ein Käufer hat, wenn mangelhafte Ware zugestellt wird, aufzählen."},
-  { image: Eight, text: "Sie können für Debit- und Kreditkarten die Unterschiede be- züglich Einsatzmöglichkeiten und Funktionsweise erklären."},
-  { image: Nine, text: "Sie können je einen Vor- und Nachteil des Barkaufs, Kredit- kaufs und des Leasings aufzählen."},
-  { image: Eleven, text: "Sie können den einfachen Wirtschaftskreislauf aufzeichnen."},
-  { image: Twelve, text: "Sie können die Auswirkung von Veränderungen von Angebot und Nachfrage auf den Preis erklären."},
-  { image: Thirteen, text: "Sie können die Begriffe BIP und Volkseinkommen definieren."},
+    { image: One, text: "Sie kennen Grundbedürfnis, Wahlbedürfnis, Individualbedürfnis und Kollektivbedürfnis voneinander unterscheiden." },
+    { image: Three, text: "Sie können die Entstehung eines Vertrages erklären." },
+    { image: Four, text: "Sie kennen vier Vertragsformen nennen."},
+    { image: Five, text: "Sie können drei Vertragsarten mit Rücktrittsrecht nennen." },
+    { image: Six, text: "Sie können das Vorgehen bei Lieferverzug und die rechtlichen Möglichkeiten bei Zahlungsverzug aufzählen."},
+    { image: Seven, text: "Sie kennen drei Forderungsmöglichkeiten, die ein Käufer hat, wenn mangelhafte Ware zugestellt wird, aufzählen."},
+    { image: Eight, text: "Sie können für Debit- und Kreditkarten die Unterschiede be- züglich Einsatzmöglichkeiten und Funktionsweise erklären."},
+    { image: Nine, text: "Sie können je einen Vor- und Nachteil des Barkaufs, Kredit- kaufs und des Leasings aufzählen."},
+    { image: Eleven, text: "Sie können den einfachen Wirtschaftskreislauf aufzeichnen."},
+    { image: Twelve, text: "Sie können die Auswirkung von Veränderungen von Angebot und Nachfrage auf den Preis erklären."},
+    { image: Thirteen, text: "Sie können die Begriffe BIP und Volkseinkommen definieren."},
+  
+  ]
 
-]
-
-function startF() {
-  setStart(true)
-
-  const min = 0;
-  const max = images_arr.length - 1;
-  const rand = min + Math.random() * (max - min);
-
-  console.log(rand)
-
-  setShow(false)
-if (rand <= max) {
-  setImage(images_arr[Math.round(rand)].image)
-  setText(images_arr[Math.round(rand)].text)
-
-}
-
-
-}
-
-
+  function startF() {
+    setStart(true)
+  
+    const min = 0;
+    const max = images_arr.length - 1;
+    const rand = min + Math.random() * (max - min);
+  
+    console.log(rand)
+  
+    setShow(false)
+  if (rand <= max) {
+    setImage(images_arr[Math.round(rand)].image)
+    setText(images_arr[Math.round(rand)].text)
+  
+  }
+  
+  
+  }
 
   function randomNumber() {
 
@@ -86,32 +82,32 @@ if (rand <= max) {
 
   }
 
-
   return (
-    <body className="flex justify-center h-screen text-gray-50 bg-gradient-to-b from-slate-900 to-stone-900 ">
+    <>
+    <div className="flex justify-center h-screen text-gray-50 bg-gradient-to-b from-slate-900 to-stone-900 ">
     {start ? (
  
-      <div className='grid mx-10'>
+ <div className='grid mx-10'>
 <div className="flex justify-center">
-      <h1 className="text-4xl text font-bold mt-20 " >Abu Grundwissen</h1>
+ <h1 className="text-4xl text font-bold mt-20 " >Abu Grundwissen</h1>
 
-      </div>
-    
+ </div>
+
 
 
 <div>
 <h2 className='text-2xl'>{text}</h2>
 
 {show ? (
-  <div className='mt-5 mb-5 w-full rounded-xl bg-slate-800 flex justify-center'>
-  <Image className="bg-slate-800 w-50  rounded-lg" onClick={() => showImage()} src={image}/>
-  </div>
+<div className='mt-5 mb-5 w-full rounded-xl bg-slate-800 flex justify-center'>
+<Image className="bg-slate-800 w-50  rounded-lg" onClick={() => showImage()} src={image}/>
+</div>
 ):(
-  <div onClick={() => showImage()} className='bg-slate-800 h-96 mt-5 mb-5 w-full rounded-xl grid place-content-center'>
-    <p>click to show</p>
-  
+<div onClick={() => showImage()} className='bg-slate-800 h-96 mt-5 mb-5 w-full rounded-xl grid place-content-center'>
+<p>click to show</p>
 
-  </div>
+
+</div>
 ) }
 <div className='flex justify-center'>
 <button onClick={() => randomNumber()} className=" bg-sky-500 hover:bg-sky-700  px-4 py-1 rounded-md text-2xl font-medium text-gray-50">Next</button>
@@ -119,16 +115,18 @@ if (rand <= max) {
 </div>
 
 </div>
-   
-  
-    ):(
-      <div className='grid'>
-      <h1 className="text-4xl text font-bold mt-20 " >Abu Grundwissen</h1>
-      <div className='flex justify-center'>
-      <button onClick={() => startF()} className="bg-sky-500 hover:bg-sky-700 h-12  px-4 py-1 rounded-md text-2xl font-medium text-gray-50">Start</button>
+
+
+):(
+ <div className='grid'>
+ <h1 className="text-4xl text font-bold mt-20 " >Abu Grundwissen</h1>
+ <div className='flex justify-center'>
+ <button onClick={() => startF()} className="bg-sky-500 hover:bg-sky-700 h-12  px-4 py-1 rounded-md text-2xl font-medium text-gray-50">Start</button>
 </div>
 </div>
-    )}
-    </body>
+)}
+    </div>
+    </>
+
   )
 }
