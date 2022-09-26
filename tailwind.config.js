a807cc4b375badc4848f7b362218c1ddc8606b95
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-09-26 15:06:05 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+       content: [
+              "./pages/**/*.{js,ts,jsx,tsx}",
+              "./components/**/*.{js,ts,jsx,tsx}",
+              "./styles/**/*.{css,scss,sass}",
+       ],
+       theme: {
+              extend: {
+
+                     colors: {
+                            'darkBlue': "#0c4a6e",
+                            'white': "#ffff",
+                            'lightBlue': "#38bdf8"
+                     },
+
+                     screens: {
+                            'md': '769px'
+                     },
+                     fontFamily: {
+                            'sans': ['Montserrat', 'sans-serif']
+
+                     }
+              },
+       },
+       plugins: [
+              require('@tailwindcss/forms'),
+              require('@tailwindcss/typography'),
+              require('@tailwindcss/aspect-ratio'),
+              require('@tailwindcss/line-clamp')
+       ],
 }
