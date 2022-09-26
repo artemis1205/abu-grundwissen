458ae20ac30de78aacd2a3070221bce0f86816b0
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-09-26 15:27:08 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-       content: [
-              "./pages/**/*.{js,ts,jsx,tsx}",
-              "./components/**/*.{js,ts,jsx,tsx}",
-              "./styles/**/*.{css,scss,sass}",
-       ],
-        theme: {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
     extend: {},
   },
- plugins: [
-    require('@tailwindcss/forms'),
-    // ...
-  ]
+  plugins: [
+  require('@tailwindcss/forms')
+  ],
 }
